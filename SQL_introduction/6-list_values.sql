f9fffaecd3041696e00ab5f8f5e7b46f8a9c9b4c
sam <sookwalinga@gmail.com>
2023-08-16 13:47:17 +0300

    refactor

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,6 +1,5 @@
 -- This SQL script lists all rows of the table first_table from the specified database.
 
--- The database name is passed as an argument to the mysql command
 -- The user is prompted to enter the password for authentication
 SELECT *
 FROM hbtn_test_db_6.first_table;
