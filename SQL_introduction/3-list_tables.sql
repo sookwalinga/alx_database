79e3b23ab18c91b83ae31318307d4d3413eaf4f7
sam <sookwalinga@gmail.com>
2023-08-16 10:16:04 +0000

    16 Aug 2023, 13:16

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,6 @@
+-- This SQL script lists all the tables of a specified database.
+
+-- The database name is passed as an argument to the mysql command
+-- It is assumed that the MySQL client command can accept arguments for the database name
+-- The user is prompted to enter the password for authentication
+SHOW TABLES FROM your_database_name;
