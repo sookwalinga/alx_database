58dc0b6b2dadc947cee021825a9e3d12c323d9e2
sam <sookwalinga@gmail.com>
2023-08-16 14:54:06 +0300

    test

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,8 +1,5 @@
--- 3-force_name.sql
-
 -- Create the table if it doesn't exist
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
 );
