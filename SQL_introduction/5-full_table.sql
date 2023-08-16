923baa410fd6a329eaca85e4de376c026a3326e1
sam <sookwalinga@gmail.com>
2023-08-16 13:37:28 +0300

    refactor

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,7 @@
 -- This SQL script prints the full description of the table first_table from the specified database.
 
--- The database name is passed as an argument to the mysql command
 -- The user is prompted to enter the password for authentication
 -- The query uses the information_schema database to retrieve table information without using DESCRIBE or EXPLAIN
 SELECT TABLE_NAME, CREATE_TABLE
 FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = 'database_name' AND TABLE_NAME = 'first_table';
+WHERE TABLE_SCHEMA = hbtn_test_db_4 AND TABLE_NAME = 'first_table';
