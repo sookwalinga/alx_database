52654e7662ef94d16aa17e46c1320b5d4628cb63
sam <sookwalinga@gmail.com>
2023-08-16 11:51:47 +0000

    16 Aug 2023, 14:51

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- 8-cities_of_california_subquery.sql
+
+-- Select cities of California using a subquery
+SELECT * FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC;
