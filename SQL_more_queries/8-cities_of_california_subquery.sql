8d0acf631133bf7692a9de7e0d88f7f90141dd78
sam <sookwalinga@gmail.com>
2023-08-16 14:57:31 +0300

    refactor

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,7 @@
 -- 8-cities_of_california_subquery.sql
 
--- Select cities of California using a subquery
-SELECT * FROM cities
+-- List cities of California using a subquery
+SELECT id, name
+FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
