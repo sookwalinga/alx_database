73869a2bc950f8146eec3a633e5d71fc76d88a62
sam <sookwalinga@gmail.com>
2023-08-16 13:32:34 +0300

    refactor

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -2,5 +2,6 @@
 
 -- The database name is passed as an argument to the mysql command
 -- The user is prompted to enter the password for authentication
+
 USE <db_name>;
-SHOW TABLES FROM `<db_name>`;
+SHOW TABLES;
