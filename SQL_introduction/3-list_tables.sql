20320a7f847b01a54df8e63699d7b100e683439e
sam <sookwalinga@gmail.com>
2023-08-16 13:29:24 +0300

    chore: update passing db name task4

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -2,4 +2,11 @@
 
 -- The database name is passed as an argument to the mysql command
 -- The user is prompted to enter the password for authentication
-SHOW TABLES FROM mysql;
+-- list_tables.sql
+
+USE <db_name>;
+
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = '<db_name>';
+
