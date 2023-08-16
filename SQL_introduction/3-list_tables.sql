227de5cb68685422b1866521878d6ef72f228678
sam <sookwalinga@gmail.com>
2023-08-16 13:34:36 +0300

    refactor

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,6 +1,5 @@
 -- This SQL script lists all the tables of a specified database.
 
--- The database name is passed as an argument to the mysql command
 -- The user is prompted to enter the password for authentication
 
 USE hbtn_test_db_0;
--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,9 +1,8 @@
 -- This SQL script creates a table called first_table in the specified database.
 
--- The database name is passed as an argument to the mysql command
 -- The user is prompted to enter the password for authentication
 -- If the table already exists, the query will not fail due to the IF NOT EXISTS clause
-CREATE TABLE IF NOT EXISTS database_name.first_table (
+CREATE TABLE IF NOT EXISTS hbtn_test_db_0.first_table (
     id INT,
     name VARCHAR(256)
 );
