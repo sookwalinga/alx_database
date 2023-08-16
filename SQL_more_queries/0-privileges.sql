dbc7ed6decbabb6bf978fc78062005f3b10489bf
sam <sookwalinga@gmail.com>
2023-08-16 11:44:29 +0000

    16 Aug 2023, 14:44

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- 0-privileges.sql
+
+-- This script should list privileges, but it won't work as intended.
+-- It doesn't grant any privileges to the users.
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
