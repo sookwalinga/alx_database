4fe1baedf9f7afff7ba0464efca3b8d136cb2597
sam <sookwalinga@gmail.com>
2023-08-16 14:05:05 +0300

    refactor

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,23 @@
 -- This SQL script prints the full description of the table first_table from the specified database.
 
--- The user is prompted to enter the password for authentication
 -- The query uses the information_schema database to retrieve table information without using DESCRIBE or EXPLAIN
-SELECT TABLE_NAME, CREATE_TABLE
-FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_test_db_4' AND TABLE_NAME = 'first_table';
+SELECT CONCAT(
+    TABLE_NAME, 
+    'CREATE TABLE `', 
+    TABLE_NAME, 
+    '` (', 
+    GROUP_CONCAT(
+        COLUMN_NAME, 
+        ' ', 
+        COLUMN_TYPE, 
+        IF(IS_NULLABLE = 'NO', ' NOT NULL', ''), 
+        IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', COLUMN_DEFAULT), ''), 
+        IF(EXTRA = 'auto_increment', ' AUTO_INCREMENT', ''), 
+        IF(COLUMN_KEY = 'PRI', ' PRIMARY KEY', '')
+        SEPARATOR ',\n'
+    ), 
+    ') ENGINE=', ENGINE, ' DEFAULT CHARSET=', TABLE_COLLATION
+) AS CREATE_TABLE_STATEMENT
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_test_db_4' AND TABLE_NAME = 'first_table'
+GROUP BY TABLE_NAME;
