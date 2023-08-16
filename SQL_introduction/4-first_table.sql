cfdb5c9cc7ec354964cd4b10b6668a03c13f45e1
sam <sookwalinga@gmail.com>
2023-08-16 10:42:41 +0000

    16 Aug 2023, 13:42

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -2,9 +2,7 @@
 
 -- The user is prompted to enter the password for authentication
 -- If the table already exists, the query will not fail due to the IF NOT EXISTS clause
-
-USE {{db_name}};
-CREATE TABLE IF NOT EXISTS first_table (
+CREATE TABLE IF NOT EXISTS hbtn_test_db_4.first_table (
     id INT,
     name VARCHAR(256)
 );
