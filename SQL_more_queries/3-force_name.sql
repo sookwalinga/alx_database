bfedf36b84660b8c15f240894af260cab544c42a
sam <sookwalinga@gmail.com>
2023-08-16 14:55:26 +0300

    refactor

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,7 @@
+-- 3-force_name.sql
+
 -- Create the table if it doesn't exist
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT,
     name VARCHAR(256) NOT NULL
 );
