7200fe3bbf7a9be1f19f0c80997f0fb80441f9ae
sam <sookwalinga@gmail.com>
2023-08-16 10:14:06 +0000

    16 Aug 2023, 13:14

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- This SQL script deletes the database hbtn_0c_0 if it exists.
+
+-- Try to drop (delete) the database hbtn_0c_0
+-- The query will attempt to drop the database, and if it doesn't exist, the query will not fail due to the IF EXISTS clause
+DROP DATABASE IF EXISTS hbtn_0c_0;
