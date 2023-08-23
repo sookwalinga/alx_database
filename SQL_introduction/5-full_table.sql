308500af51f154bca1a726dbdaef6e3a3fbafdb7
sam <sookwalinga@gmail.com>
2023-08-23 14:55:46 +0300

    refactor

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,11 @@
--- This SQL script prints the full description of the table first_table from the specified database.
+-- This SQL script prints the full description of the table
+-- first_table from the specified database.
 
 -- The user is prompted to enter the password for authentication
--- The query uses the information_schema database to retrieve table information without using DESCRIBE or EXPLAIN
-SELECT TABLE_NAME, CREATE_TABLE
-FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_test_db_4' AND TABLE_NAME = 'first_table';
+-- The query uses the information_schema database to retrieve
+-- table information without using DESCRIBE or EXPLAIN
+SELECT column_name, column_type
+FROM information_schema.columns
+WHERE table_name = 'first_table'
+AND table_schema = DATABASE();
+
