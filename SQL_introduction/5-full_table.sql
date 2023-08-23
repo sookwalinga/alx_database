755be90a53f29d66affb22cc088ec67f6ded4d87
sam <sookwalinga@gmail.com>
2023-08-23 14:58:19 +0300

    refactor

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -4,7 +4,8 @@
 -- The user is prompted to enter the password for authentication
 -- The query uses the information_schema database to retrieve
 -- table information without using DESCRIBE or EXPLAIN
-SELECT table_name, create_statement
+SELECT create_statement
 FROM information_schema.tables
 WHERE table_name = 'first_table'
 AND table_schema = DATABASE();
+
